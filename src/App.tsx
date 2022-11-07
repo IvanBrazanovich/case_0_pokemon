b09b59617b15ccb7d55ed4cb39a834df2b3ad4d5
Ivan <ivanbrazas@gmail.com>
2022-11-07 17:07:19 -0300

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,42 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 import api from "./api";
-import {Pokemon} from "./types";
+import { Pokemon } from "./types";
 
 function App() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
+  const [carrito, setCarrito] = useState<Pokemon[]>([]);
   useEffect(() => {
     api.list().then(setPokemons);
   }, []);
 
+  if (pokemons?.length === 0) return <h1> Cargando... </h1>;
   return (
     <>
       <section>
         {pokemons.map((pokemon) => (
           <article key={pokemon.id}>
-            <img className="nes-container" src={pokemon.image} />
+            <img alt="imagen" className="nes-container" src={pokemon.image} />
             <div>
-              <p>{pokemon.name}</p>
+              <p>
+                {pokemon.name}: <span>${pokemon.price} </span>{" "}
+              </p>
               <p>{pokemon.description}</p>
             </div>
-            <button className="nes-btn">Agregar</button>
+            <button
+              onClick={() => {
+                if (carrito?.length === 3) return;
+                setCarrito([...carrito, pokemon]);
+              }}
+              className="nes-btn"
+            >
+              Agregar
+            </button>
           </article>
         ))}
       </section>
       <aside>
-        <button className="nes-btn is-primary">0 items</button>
+        <button className="nes-btn is-primary">{carrito?.length} items</button>
       </aside>
     </>
   );
